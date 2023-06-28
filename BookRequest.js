e7c798c9810e1a10987d938e8515c390cbb50bc4
XxatharvaxX <86345529+XxatharvaxX@users.noreply.github.com>
2023-06-28 18:42:17 +1200

    Add files via upload

---
+++ BookRequest.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import {View, StyleSheet,KeyboardAvoidingView, FlatList, Alert, SafeAreaView, Text} from "react-native";
+
+export default class BookRequestScreen extends Component{
+    render(){
+        return(
+            <View>
+                <Text>Book Request Screen</Text>
+            </View>
+        )
+    }
+}
